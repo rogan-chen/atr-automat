e78597c310dcf85576aa07d1324f428242f6f4c1
px.chen <px.chen@astrotech.com.hk>
2023-02-08 10:09:47 +0800

    厂商管理-修改

--- src/view/commodity-management/manufacturer-management/manufacturer-management.vue
+++ src/view/commodity-management/manufacturer-management/manufacturer-management.vue
@@ -4,8 +4,7 @@
       <tables ref="tables" editable searchable search-place="top" v-model="tableData" :columns="columns"
         @on-delete="handleDelete" />
       <Button style="margin: 10px 0;" type="primary" @click="exportExcel">导出为excel文件</Button>
-      <Button style="position:absolute; top: 27px; right: 17px;" type="primary"
-        @click="addCommodityType">新增厂商</Button>
+      <Button style="position:absolute; top: 27px; right: 17px;" type="primary" @click="addCommodityType">新增厂商</Button>
     </Card>
     <Modal v-model="visible" :title="title" :mask-closable="false" @on-ok="okModal" @on-cancel="cancelModal">
       <div>
@@ -49,8 +48,6 @@
 import Mock from 'mockjs'
 import Tables from '_c/tables'
 
-const Random = Mock.Random
-
 // TODO: 厂商管理添加修改功能；
 export default {
   name: 'commodity_type_page',
@@ -62,31 +59,55 @@ export default {
       columns: [
         { title: '编号', key: 'number', sortable: true },
         { title: '厂商名称', key: 'name', editable: true },
-        { title: '地址', key: 'address', editable: true },
-        { title: '电话号码', key: 'phoneNumber', editable: true },
-        { title: '联系人', key: 'contacts', editable: true },
+        { title: '地址', key: 'businessAddress', editable: true },
+        { title: '电话号码', key: 'contactNumber', editable: true },
+        { title: '联系人', key: 'contact', editable: true },
         // { title: '传真', key: 'fax', editable: true },
         { title: 'Email', key: 'email', editable: true },
         {
           title: '操作',
-          key: 'handle',
-          options: ['delete'],
-          button: [
-            (h, params, vm) => {
-              return h('Poptip', {
+          key: 'action',
+          width: 140,
+          render: (h, params) => {
+            return h('div', [
+              h('Button', {
                 props: {
-                  confirm: true,
-                  title: '你确定要删除吗?'
+                  type: 'info',
+                  size: 'small',
+                  ghost: true,
                 },
                 on: {
-                  'on-ok': () => {
-                    vm.$emit('on-delete', params)
-                    vm.$emit('input', params.tableData.filter((item, index) => index !== params.row.initRowIndex))
+                  click: () => {
+                    this.title = '修改厂商';
+                    this.visible = true;
+                    this.number = params.row.number;
+                    this.name = params.row.name;
+                    this.contractNumber = params.row.contractNumber;
+                    this.dutyNumber = params.row.dutyNumber;
+                    this.postalCode = params.row.postalCode;
+                    this.telephoneNumber = params.row.telephoneNumber;
+                    this.contact = params.row.contact;
+                    this.email = params.row.email;
+                    this.contactNumber = params.row.contactNumber;
+                    this.businessAddress = params.row.businessAddress;
+                    this.registeAddress = params.row.registeAddress;
+                    this.machineGroupIndex = params.row.initRowIndex;
                   }
                 }
-              }, [])
-            },
-          ]
+              }, '修改'),
+              h('span', {}, ' '),
+              h('Button', {
+                props: {
+                  type: 'warning',
+                  size: 'small',
+                  ghost: true,
+                },
+                on: {
+                  click: () => this.tableData.splice(params.index, 1)
+                },
+              }, '删除')
+            ]);
+          }
         }
       ],
       tableData: [],
@@ -104,6 +125,7 @@ export default {
       contactNumber: '', // 联系电话
       businessAddress: '', // 营业地址
       registeAddress: '', // 注册地址
+      machineGroupIndex: -1,
     }
   },
   methods: {
@@ -128,20 +150,54 @@ export default {
       this.contactNumber = '';
       this.businessAddress = '';
       this.registeAddress = '';
+      this.machineGroupIndex = -1;
     },
     okModal() {
-      this.tableData.unshift({
-        id: this.tableData.length + 1,
+      // 新增
+      if (this.machineGroupIndex < 0) {
+        this.tableData.unshift({
+          id: this.tableData.length + 1,
+          number: this.number,
+          name: this.name,
+          contractNumber: this.contractNumber,
+          dutyNumber: this.dutyNumber,
+          postalCode: this.postalCode,
+          telephoneNumber: this.telephoneNumber,
+          contact: this.contact,
+          email: this.email,
+          contactNumber: this.contactNumber,
+          businessAddress: this.businessAddress,
+          registeAddress: this.registeAddress,
+        });
+        return;
+      }
+
+      // 修改
+      this.tableData.splice(this.machineGroupIndex, 1, {
+        id: this.tableData[this.machineGroupIndex].id,
         number: this.number,
         name: this.name,
-        address: this.businessAddress,
-        phoneNumber: this.contactNumber,
-        contacts: this.contact,
+        contractNumber: this.contractNumber,
+        dutyNumber: this.dutyNumber,
+        postalCode: this.postalCode,
+        telephoneNumber: this.telephoneNumber,
+        contact: this.contact,
         email: this.email,
-      })
+        contactNumber: this.contactNumber,
+        businessAddress: this.businessAddress,
+        registeAddress: this.registeAddress,
+      });
     },
     cancelModal() {
-      
+
+    },
+    getRandomStr(len) {
+      var x = "123456789poiuytrewqasdfghjklmnbvcxzQWERTYUIPLKJHGFDSAZXCVBNM";
+      var str = "";
+      for (var i = 0; i < len; i++) {
+        str += x.charAt(Math.ceil(Math.random() * 100000000) % x.length);
+      }
+      return str;
     },
   },
   mounted() {
@@ -158,11 +214,16 @@ export default {
           '广州统一企业有限公司',
           '元气森林股份有限公司',
         ],
-        'address': '@city(true)',
-        'phoneNumber': /^1(3|5|7|8)[0-9]{9}$/,
-        'contacts': '@cname()',
+        'contactNumber': /^1(3|5|7|8)[0-9]{9}$/,
+        'contact': '@cname()',
         'fax': '',
         'email': '@email()',
+        'contractNumber': this.getRandomStr(16),
+        'dutyNumber': this.getRandomStr(15),
+        'postalCode': '@zip()',
+        'telephoneNumber': /^1(3|5|7|8)[0-9]{9}$/,
+        'businessAddress': '@city(true)',
+        'registeAddress': '@city(true)',
       }],
     });
 
