3ce913d9764826b30eb9295b3c3815ef841d1f82
px.chen <px.chen@astrotech.com.hk>
2023-02-22 09:10:46 +0800

    设置支付参数->支付宝参数设置

--- src/view/system-setting/pay-parameter/pay-parameter.vue
+++ src/view/system-setting/pay-parameter/pay-parameter.vue
@@ -78,7 +78,93 @@
         </div>
       </Card>
     </TabPane>
-    <TabPane label="支付宝参数设置" name="支付宝参数设置">支付宝参数设置</TabPane>
+    <TabPane label="支付宝参数设置" name="支付宝参数设置">
+      <Card>
+        <template #title>
+          <strong>支付宝参数设置</strong>
+        </template>
+        <Row class="row">
+          <Col span="12" class="title">
+          <strong>是否启用支付宝：</strong>
+          </Col>
+          <Col span="12">
+          <Select v-model="alipayParams.enable" placeholder="请选择是否启用支付宝" class="form">
+            <Option v-for="item in whetherList" :value="item.value" :key="item.value">{{ item.label }}</Option>
+          </Select>
+          </Col>
+        </Row>
+        <Row class="row">
+          <Col span="12" class="title">
+          <strong>支付宝绑定模式：</strong>
+          </Col>
+          <Col span="12">
+          <Select v-model="alipayParams.bindModel" placeholder="请选择支付宝绑定模式" class="form">
+            <Option :value="alipayParams.bindModel" :key="alipayParams.bindModel">{{ alipayParams.bindModel }}</Option>
+          </Select>
+          </Col>
+        </Row>
+        <Row class="row">
+          <Col span="12" class="title">
+          <strong>支付宝折扣：</strong>
+          </Col>
+          <Col span="12">
+          <Tooltip content="默认100不打折" placement="right-start">
+            <InputNumber class="form" :max="100" :min="1" v-model="alipayParams.discount"
+              :formatter="value => `${value}%`" />
+          </Tooltip>
+          </Col>
+        </Row>
+        <Row class="row">
+          <Col span="12" class="title">
+          <strong>支付宝参数设置：</strong>
+          </Col>
+          <Col span="12">
+          <Button type="primary" @click="onAlipayScan">点击此处扫码设置支付宝参数</Button>
+          </Col>
+        </Row>
+        <Row class="row">
+          <Col span="12" class="title">
+          <strong>支付宝账户名称：</strong>
+          </Col>
+          <Col span="12">
+          <Input v-model="alipayParams.accountName" placeholder="请输入支付宝账户名称" class="form" />
+          </Col>
+        </Row>
+        <Row class="row">
+          <Col span="12" class="title">
+          <strong>支付宝商户号：</strong>
+          </Col>
+          <Col span="12">
+          <Input v-model="alipayParams.merchantID" placeholder="请输入支付宝商户号" class="form" />
+          </Col>
+        </Row>
+        <Row class="row">
+          <Col span="12" class="title">
+          <strong>支付宝账户：</strong>
+          </Col>
+          <Col span="12">
+          <Input v-model="alipayParams.account" placeholder="请输入支付宝账户" class="form" />
+          </Col>
+        </Row>
+        <Row class="row">
+          <Col span="12" class="title">
+          <strong>客服电话：</strong>
+          </Col>
+          <Col span="12">
+          <Input v-model="alipayParams.servicePhone" placeholder="请输入客服电话" class="form" />
+          </Col>
+        </Row>
+        <div class="band">
+          <strong class="bandTitle">直付通收款，收取6‰手续费，请慎重填写并认真核对，提交后不支持修改!</strong>
+          <Button class="bandBtn" type="primary" @click="onAlipayBind">绑定</Button>
+        </div>
+      </Card>
+      <Modal mask :closable="false" cancel-text=" " ok-text="扫描完成" v-model="alipayParams.visible" title="扫码设置支付宝参数">
+        <p class="bandTitle">链接地址：http://www.tcnvmms.com/zftjoin/zftbind?sstid=59820</p>
+        <img class="qrcode-img" src="https://www.liantu.com/images/2013/liantu.png">
+        <p class="bandTitle">用申请成功的支付宝账号扫描上方二维码，扫描完成后再点击页面下方“绑定”按钮完成操作！</p>
+      </Modal>
+    </TabPane>
     <TabPane label="刷脸参数设置" name="刷脸参数设置">刷脸参数设置</TabPane>
     <TabPane label="京东参数设置" name="京东参数设置">京东参数设置</TabPane>
     <TabPane label="翼支付参数设置" name="翼支付参数设置">翼支付参数设置</TabPane>
@@ -101,7 +187,7 @@ export default {
         enable: '是', // 是否启用
         disable: false, // 是否禁用
         bindModel: '云商服务器', // 绑定模式
-        discount: '100', // 折扣
+        discount: 100, // 折扣
         merchantID: '1626599950', // 微信商户号
         name: '林荣', // 姓名
         bankAccount: '工商银行 6222***********3820', // 银行账号
@@ -111,12 +197,13 @@ export default {
       // 支付宝参数设置
       alipayParams: {
         enable: '是', // 是否启用
-        bindModel: '云商服务器', // 绑定模式
-        discount: '100', // 折扣
+        bindModel: '直付通', // 绑定模式
+        discount: 100, // 折扣
         accountName: '林荣', // 账户名称
         merchantID: '2088610367330373', // 商户ID
         account: '18929455118', // 账号
         servicePhone: '18929455118', // 客服电话
+        visible: false, // 扫码设置支付宝参数
       },
 
       // 刷脸参数设置
@@ -192,6 +279,32 @@ export default {
       }
       this.$Message.info('绑定成功！');
     },
+
+    // 支付宝参数设置 -> 扫码
+    onAlipayScan() {
+      this.alipayParams.visible = true;
+      console.log(213)
+    },
+    // 支付宝参数设置 -> 绑定
+    onAlipayBind() {
+      if (this.alipayParams.accountName.length === 0) {
+        this.$Message.error('支付宝账号名称不能为空');
+        return;
+      }
+      if (this.alipayParams.merchantID.length === 0) {
+        this.$Message.error('支付宝商户号不能为空');
+        return;
+      }
+      if (this.alipayParams.account.length === 0) {
+        this.$Message.error('支付宝账号不能为空');
+        return;
+      }
+      if (this.alipayParams.servicePhone.length === 0) {
+        this.$Message.error('客服电话不能为空');
+        return;
+      }
+      this.$Message.info('直付通绑定成功！');
+    },
   },
 }
 </script>
@@ -227,10 +340,18 @@ export default {
 
 .bandTitle {
   color: red;
+  font-size: 15px;
 }
 
 .bandBtn {
   margin-top: 10px;
   width: 100px;
+}
+
+.qrcode-img {
+  width: 240px;
+  height: 240px;
+  margin-top: 15px;
+  margin-bottom: 15px;
 }
 </style>
