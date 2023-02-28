30f51326da21357ab3139911433d8cabcadac560
px.chen <px.chen@astrotech.com.hk>
2023-02-28 09:22:01 +0800

    历史记录-补货记录

--- src/view/history-record/replenish-record/replenish-record.vue
+++ src/view/history-record/replenish-record/replenish-record.vue
@@ -3,33 +3,21 @@
     <Card>
       <tables ref="tables" editable searchable search-place="top" v-model="tableData" :columns="columns" />
       <Button style="margin: 10px 0;" type="primary" @click="exportExcel">导出为excel文件</Button>
-      <Button style="position:absolute; top: 27px; right: 17px;" type="primary" @click="addMachineGroup">新增记录</Button>
     </Card>
-    <Modal v-model="visible" :title="title" :mask-closable="false" @on-ok="okModal" @on-cancel="cancelModal">
-      <div style="text-indent: 12px;">
-        机器组名：
-        <Select v-model="machineGroupName" placeholder="请选择机器分组" style="width: 400px;">
-          <Option v-for="item in machineGroupNameList" :value="item.value" :key="item.value">{{ item.label }}</Option>
-        </Select>
-      </div>
-      <div style="margin-top: 20px; text-indent: 12px;">
-        机器编号：
-        <Select v-model="machineNumber" placeholder="请选择机器编号" style="width: 400px;">
-          <Option v-for="item in machineNumberList" :value="item.value" :key="item.value">{{ item.label }}</Option>
-        </Select>
-      </div>
-      <div style="margin-top: 20px;">
-        机器名： <Input v-model="machineName" placeholder="请输入机器名" style="width: 400px;" />
-      </div>
-      <div style="margin-top: 20px;">
-        部署地址： <Input v-model="deployAddress" placeholder="请输入部署地址" style="width: 400px;" />
-      </div>
-      <div style="margin-top: 20px; text-indent: 12px;">
-        操作类型：
-        <Select v-model="handleType" placeholder="请选择操作类型" style="width: 400px;">
-          <Option v-for="item in doorList" :value="item.value" :key="item.value">{{ item.label }}</Option>
-        </Select>
-      </div>
+    <Modal width="1000" v-model="visible" :title="title" :mask-closable="false" ok-text="关闭" cancel-text=" " @on-ok="okModal" @on-cancel="cancelModal">
+      <Row>
+        <Col span="8">
+        <strong style="font-size:15px;">补货人：{{ replenishMan }}</strong>
+        </Col>
+        <Col span="8">
+        <strong style="font-size:15px;">补货数量：{{ replenishCount }}</strong>
+        </Col>
+        <Col span="8">
+        <strong style="font-size:15px;">补货时间：{{ replenishDate }}</strong>
+        </Col>
+      </Row>
+      <tables style="margin-top:14px;" ref="detailTables" v-model="detailTable" :columns="detailColumns" />
+      <Button style="margin-top: 10px;" type="primary" @click="exportDetailExcel">补货详情excel导出</Button>
     </Modal>
   </div>
 </template>
@@ -43,7 +31,7 @@ import {
   machineNumberList,
   machineNumberValues,
   doorList,
-  doorValues,
+  commoditys,
 } from '@/mock/data/option-data.js';
 
 export default {
@@ -57,9 +45,11 @@ export default {
         { title: '机器组名', key: 'machineGroupName' },
         { title: '机器编号', key: 'machineNumber', sortable: true },
         { title: '机器名', key: 'machineName', editable: true },
+        { title: '补货次数', key: 'replenishTimes' },
+        { title: '补货数量', key: 'replenishCount' },
         { title: '部署地址', key: 'deployAddress', editable: true },
-        { title: '操作类型', key: 'handleType' },
-        { title: '操作时间', key: 'handleTime' },
+        { title: '补货时间', key: 'replenishDate' },
+        { title: '补货人', key: 'replenishMan' },
         {
           title: '操作',
           key: 'action',
@@ -74,18 +64,23 @@ export default {
                 },
                 on: {
                   click: () => {
-                    this.title = '修改记录';
                     this.visible = true;
-                    this.machineGroupName = params.row.machineGroupName;
-                    this.machineNumber = params.row.machineNumber;
-                    this.machineName = params.row.machineName;
-                    this.deployAddress = params.row.deployAddress;
-                    this.handleType = params.row.handleType;
-                    this.handleTime = params.row.handleTime;
-                    this.machineGroupIndex = params.row.initRowIndex;
+                    this.replenishDate = params.row.replenishDate;
+                    this.replenishMan = params.row.replenishMan;
+                    this.replenishCount = params.row.replenishCount;
+                    this.detailTable = Mock.mock({
+                      'list|3-8': [{
+                        'id|+1': 1,
+                        'freightWay|+1': 1,
+                        'commodityName|1': commoditys,
+                        'replenishCountBefore|1-50': 1,
+                        'replenishCountAfter|60-100': 1,
+                        'replenishCountWait|1-20': 1,
+                      }]
+                    }).list;
                   }
                 }
-              }, '修改'),
+              }, '详情'),
               h('span', {}, ' '),
               h('Button', {
                 props: {
@@ -105,76 +100,71 @@ export default {
       machineGroupNameList,
       machineNumberList,
       doorList,
-      // 新增/修改机器
-      title: '新增机器',
+      // 补货详情
+      title: '补货详情',
       visible: false,
-      machineGroupName: '', // 机器组名
-      machineNumber: '',  // 机器编号
-      machineName: '', // 机器名
-      deployAddress: '', // 部署地址
-      handleType: '', // 操作类型
-      handleTime: '', // 操作时间
-      machineGroupIndex: -1,
+      replenishDate: '',
+      replenishMan: '',
+      replenishCount: '',
+      detailTable: [],
+      detailColumns: [
+        { title: '货道', key: 'freightWay' },
+        { title: 'SKU/商品名称', key: 'commodityName' },
+        { title: '补货前数量', key: 'replenishCountBefore' },
+        { title: '补货后数量', key: 'replenishCountAfter' },
+        { title: '待补货数量', key: 'replenishCountWait' },
+        {
+          title: '操作',
+          key: 'action',
+          width: 140,
+          render: (h, params) => {
+            return h('div', [
+              h('Button', {
+                props: {
+                  type: 'warning',
+                  size: 'small',
+                  ghost: true,
+                },
+                on: {
+                  click: () => this.detailTable.splice(params.index, 1)
+                },
+              }, '删除')
+            ]);
+          }
+        }
+      ],
     }
   },
   methods: {
     exportExcel() {
       this.$refs.tables.exportCsv({
-        filename: `开关门记录-${(new Date()).valueOf()}.csv`
+        filename: `补货记录-${(new Date()).valueOf()}.csv`
       })
     },
-    addMachineGroup() {
-      this.title = '新增记录';
-      this.visible = true;
-      this.machineGroupName = '';
-      this.machineNumber = '';
-      this.machineName = '';
-      this.deployAddress = '';
-      this.handleType = '';
-      this.handleTime = '';
-      this.machineGroupIndex = -1;
-    },
-    okModal() {
-      // 新增
-      if (this.machineGroupIndex < 0) {
-        this.tableData.unshift({
-          id: this.tableData.length + 1,
-          machineGroupName: this.machineGroupName,
-          machineNumber: this.machineNumber,
-          machineName: this.machineName,
-          deployAddress: this.deployAddress,
-          handleType: this.handleType,
-          handleTime: new Date().toLocaleString().replace('/', '-').replace('/', '-'),
-        });
-        return;
-      }
 
-      // 修改
-      this.tableData.splice(this.machineGroupIndex, 1, {
-        id: this.tableData[this.machineGroupIndex].id,
-        machineGroupName: this.machineGroupName,
-        machineNumber: this.machineNumber,
-        machineName: this.machineName,
-        deployAddress: this.deployAddress,
-        handleType: this.handleType,
-        handleTime: new Date().toLocaleString().replace('/', '-').replace('/', '-'),
-      });
+    exportDetailExcel() {
+      this.$refs.detailTables.exportCsv({
+        filename: `补货详情-${(new Date()).valueOf()}.csv`
+      })
     },
-    cancelModal() {
 
-    },
+    okModal() { },
+
+    cancelModal() { },
   },
   mounted() {
     // mock data
     const mockData = Mock.mock({
-      'list|3-8': [{
+      'list|5-10': [{
         'id|+1': 1,
-        'machineGroupName|+1':machineGroupNameValues,
+        'machineGroupName|+1': machineGroupNameValues,
         'machineNumber|1': machineNumberValues,
         'machineName': '@title(1)',
+        'replenishTimes|1-10': 1,
+        'replenishCount|10-50': 1,
         'deployAddress': '@city()',
-        'handleType|1': doorValues,
-        'handleTime': '@datetime()',
+        'replenishDate': '@datetime()',
+        'replenishMan': '@cname()',
       }],
     });
 
@@ -183,6 +173,4 @@ export default {
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
